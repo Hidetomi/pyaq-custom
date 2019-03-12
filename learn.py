4f6b7b1f42acca1f0e6fa14b8ebc82612b0630f0
Hidetomi <umachi.0403@gmail.com>
2019-03-12 21:35:32 +0900

    sgfモジュールを使用するように変更。

--- learn.py
+++ learn.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sys import stdout
-import time
-from board import BVCNT, BSIZE, FEATURE_CNT
-import model
-import numpy as np
-from sgf import sgf2feed, import_sgf
 import tensorflow as tf
+import numpy as np
+import model
+
+from sys import stdout
+from board import BSIZE, BVCNT, FEATURE_CNT
+from sgf_reader import sgf2feed, import_sgf
+from tqdm import trange
+from parameters import PARAMS
+
 
 rnd_array = [np.arange(BVCNT + 1)]
 for i in range(1, 8):
@@ -66,7 +69,7 @@ def average_gradients(tower_grads):
 
 def stdout_log(str):
     stdout.write(str)
-    log_file = open("log.txt", "a")
+    log_file = open("learning-rate.log", "a")
     log_file.write(str)
     log_file.close()
 
@@ -91,6 +94,7 @@ def learn(lr_=1e-4, dr_=0.7, sgf_dir="sgf/", use_gpu=True, gpu_cnt=1):
 
         lr = tf.placeholder(tf.float32, shape=[], name="learning_rate")
 
+        # Adamアルゴリズム,lr=学習率
         opt = tf.train.AdamOptimizer(lr)
         dn = model.DualNetwork()
 
@@ -104,8 +108,9 @@ def learn(lr_=1e-4, dr_=0.7, sgf_dir="sgf/", use_gpu=True, gpu_cnt=1):
                         f_list[gpu_idx], temp=1.0, dr=dr_)
                     policy_ = tf.clip_by_value(policy_, 1e-6, 1)
 
-                    loss_p = -tf.reduce_mean(tf.log(
-                        tf.reduce_sum(tf.multiply(m_list[gpu_idx], policy_), 1)))
+                    loss_p = - \
+                        tf.reduce_mean(tf.log(tf.reduce_sum(
+                            tf.multiply(m_list[gpu_idx], policy_), 1)))
                     loss_v = tf.reduce_mean(
                         tf.square(tf.subtract(value_, r_list[gpu_idx])))
                     if gpu_idx == 0:
@@ -119,45 +124,47 @@ def learn(lr_=1e-4, dr_=0.7, sgf_dir="sgf/", use_gpu=True, gpu_cnt=1):
         train_op = opt.apply_gradients(average_gradients(tower_grads))
 
         # calculate accuracy
-        with tf.variable_scope(tf.get_variable_scope(), reuse=True):
-            with tf.device("/%s:0" % device_name):
-                f_acc = tf.placeholder(
-                    "float", shape=[None, BVCNT, FEATURE_CNT], name="feature_acc")
-                m_acc = tf.placeholder(
-                    "float", shape=[None, BVCNT + 1], name="move_acc")
-                r_acc = tf.placeholder(
-                    "float", shape=[None], name="result_acc")
+        with tf.variable_scope(tf.get_variable_scope(), reuse=True), \
+                tf.device("/%s:0" % device_name):
 
-                p_, v_ = dn.model(f_acc, temp=1.0, dr=1.0)
-                prediction = tf.equal(tf.reduce_max(p_, 1),
-                                      tf.reduce_max(tf.multiply(p_, m_acc), 1))
-                accuracy_p = tf.reduce_mean(tf.cast(prediction, "float"))
-                accuracy_v = tf.reduce_mean(tf.square(tf.subtract(v_, r_acc)))
-                accuracy = (accuracy_p, accuracy_v)
+            f_acc = tf.placeholder(
+                "float", shape=[None, BVCNT, FEATURE_CNT], name="feature_acc")
+            m_acc = tf.placeholder(
+                "float", shape=[None, BVCNT + 1], name="move_acc")
+            r_acc = tf.placeholder(
+                "float", shape=[None], name="result_acc")
+
+            p_, v_ = dn.model(f_acc, temp=1.0, dr=1.0)
+            prediction = tf.equal(tf.reduce_max(p_, 1),
+                                  tf.reduce_max(tf.multiply(p_, m_acc), 1))
+            accuracy_p = tf.reduce_mean(tf.cast(prediction, "float"))
+            accuracy_v = tf.reduce_mean(tf.square(tf.subtract(v_, r_acc)))
+            accuracy = (accuracy_p, accuracy_v)
 
         sess = dn.create_sess()
 
     # load sgf and convert to feed
-    sgf_list = import_sgf(sgf_dir)
-    sgf_cnt = len(sgf_list)
-    stdout_log("imported %d sgf files.\n" % sgf_cnt)
-    sgf_train = [sgf_list[i] for i in range(sgf_cnt) if i % 100 != 0]  # 99%
-    sgf_test = [sgf_list[i] for i in range(sgf_cnt) if i % 100 == 0]  # 1%
+    games, all_games_total_tempo = import_sgf(sgf_dir)
+    games_count = len(games)
+    # stdout_log("imported %d sgf files.\n" % games_count)
+    sgf_train = [games[i] for i in range(games_count) if i % 100 != 0]  # 99%
+    sgf_test = [games[i] for i in range(games_count) if i % 100 == 0]  # 1%
 
-    stdout.write("converting ...\n")
-    feed = [Feed(*(sgf2feed(sgf_train))), Feed(*(sgf2feed(sgf_test)))]
+    # stdout.write("converting ...\n")
+    feed = [Feed(*(sgf2feed(sgf_train, all_games_total_tempo))),
+            Feed(*(sgf2feed(sgf_test, all_games_total_tempo)))]
     feed_cnt = feed[0].size
 
     # learning settings
-    batch_cnt = 128
-    total_epochs = 8 * 5
+    batch_cnt = PARAMS.get('batch_cnt')
+    total_epochs = PARAMS.get('total_epochs')
     epoch_steps = feed_cnt // (batch_cnt * gpu_cnt)
-    total_steps = total_epochs * epoch_steps
+    # total_steps = total_epochs * epoch_steps
     global_step_idx = 0
     learning_rate = lr_
 
     stdout_log("learning rate=%.1g\n" % (learning_rate))
-    start_time = time.time()
+    # start_time = time.time()
 
     # training
     for epoch_idx in range(total_epochs):
@@ -165,7 +172,7 @@ def learn(lr_=1e-4, dr_=0.7, sgf_dir="sgf/", use_gpu=True, gpu_cnt=1):
             learning_rate *= 0.5
             stdout_log("learning rate=%.1g\n" % (learning_rate))
 
-        for step_idx in range(epoch_steps):
+        for step_idx in trange(epoch_steps, desc="Training....."):
             feed_dict_ = {}
             feed_dict_[lr] = learning_rate
             for gpu_idx in range(gpu_cnt):
@@ -177,46 +184,13 @@ def learn(lr_=1e-4, dr_=0.7, sgf_dir="sgf/", use_gpu=True, gpu_cnt=1):
             sess.run(train_op, feed_dict=feed_dict_)
             global_step_idx += 1
 
-            if global_step_idx % (total_steps // 1000) == 0:
-                progress_now = float(global_step_idx) / total_steps * 100
-                str_log = "progress: %03.2f[%%] " % (progress_now)
-
-                elapsed_time = time.time() - start_time
-                str_log += "%03.1f" % (elapsed_time) + "[sec]"
-                stdout_log("%s\n" % (str_log))
-                start_time = time.time()
-
-#                 if global_step_idx % 10 == 0:
-#                     dn.save_vars(sess, "model.ckpt")
-
-        str_log = ""
-        # str_summary = "%3.3f" % (float(global_step_idx) / total_steps * 100)
         acc_steps = feed[1].size // batch_cnt
         np.random.shuffle(feed[0]._perm)
-        for i in range(2):
-            acc_str = "train" if i == 0 else "test "
-            acc_sum = [0.0, 0.0]
 
-            for _ in range(acc_steps):
-                acc_batch = feed[i].next_batch(batch_cnt)
-                accur = sess.run(
-                    accuracy, feed_dict={f_acc: acc_batch[0],
-                                         m_acc: acc_batch[1],
-                                         r_acc: acc_batch[2]})
-                acc_sum[0] += accur[0]
-                acc_sum[1] += accur[1]
-
-            str_log += "%s: policy=%3.2f[%%]  value=%.3f\n" \
-                % (acc_str,
-                   acc_sum[0] / acc_steps * 100,
-                   acc_sum[1] / acc_steps / 2)
-            # str_summary += "\t%3.3f\t%3.3f" \
-            #     % (acc_sum[0] / acc_steps * 100,
-            #        acc_sum[1] / acc_steps / 2)
-
-        stdout_log("%s\n" % (str_log))
-        # log_file = open("log_summary.txt", "aw")
-        # log_file.write("%s\n" % (str_summary))
-        # log_file.close()
+        for _ in range(acc_steps):
+            acc_batch = feed[i].next_batch(batch_cnt)
+            sess.run(accuracy, feed_dict={f_acc: acc_batch[0],
+                                          m_acc: acc_batch[1],
+                                          r_acc: acc_batch[2]})
 
     dn.save_vars(sess, "model.ckpt")
